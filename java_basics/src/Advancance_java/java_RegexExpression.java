6aaf921b8f74804a144546b7e232633bde91a18e
pruthvi28994 <pruthvi28994@gmail.com>
2021-06-08 16:12:39 +0530

    added networks and images

---
+++ java_basics/src/Advancance_java/java_RegexExpression.java
@@ -0,0 +1,35 @@
+package Advancance_java;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class java_RegexExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		[abc]-->a or b or c
+//		[^abc]--> except abc
+//		[a-z] --> character within a-z
+//		$-->end
+//		.abc-->if you use (.) before or after then it neglects all the character before or after
+//		[] {1}-->{} use specify number of character
+//		^--->start
+		Scanner sc=new Scanner(System.in);
+		String USNmatch="^[1][a-zA-Z]{2}[0-9]{2}[a-zA-Z]{2}[0-9]{3}$";
+		//make use of Pattern to check your regexpression
+		Pattern regex=Pattern.compile(USNmatch);
+		String USN;
+		System.out.println("Regular Expression");
+		System.out.println("1-->enter the USN:");
+		USN=sc.nextLine();
+		
+		//matcher helps to match with your input text
+		Matcher m=regex.matcher(USN);
+		System.out.println("Details of Pattern are :"+m);
+		//find() returns boolean if true
+		boolean status=m.find();
+		System.out.println("USN is in "+status+" format");
+		
+	}
+
+}
